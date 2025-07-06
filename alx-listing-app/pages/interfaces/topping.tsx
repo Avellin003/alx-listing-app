7e1989c408c3304088ad7521a0c8613f1c2afa37
Avellin003 <uavellinbonaparte@gmail.com>
2025-07-06 18:17:17 +0200

    milestone1

---
+++ alx-listing-app/pages/interfaces/topping.tsx
@@ -0,0 +1,9 @@
+export default function topping() {
+  return (
+    <div className="bg-[#11986d] flex flex-row p-2 justify-center items-center text-white space-x-6 text-center text-xs">
+      <p>Overseas trip? </p>
+      <p>Get the latest information on travel guides</p>
+      <button className="bg-black p-2 rounded-3xl">More info</button>
+    </div>
+  );
+}
